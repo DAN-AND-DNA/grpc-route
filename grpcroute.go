4fa6cc23ec725975286e088f9923ec4454e9d63b
io-cat <5471174+io-cat@user.noreply.gitee.com>
2022-11-17 10:51:41 +0800

    add Grpc stream

--- grpcroute.go
+++ grpcroute.go
@@ -8,10 +8,17 @@ import (
 )
 
 type HandleProto func(context.Context, interface{}) (interface{}, error)
+type HandleProtoStream func(grpc.ServerStream) error
 
 type Option interface {
 	GetHandler(string) (HandleProto, bool) // FullMethod is the full RPC method string, i.e.,  /package.service/method
 	SetHandler(string, HandleProto)
+	RemoveHandler(string)
+}
+
+type OptionStream interface {
+	GetHandler(string) (HandleProtoStream, bool) // FullMethod is the full RPC method string, i.e.,  /package.service/method
+	SetHandler(string, HandleProtoStream)
 	RemoveHandler(string)
 }
 
@@ -26,5 +33,19 @@ func GrpcRoute(option Option) grpc.UnaryServerInterceptor {
 		}
 
 		return nil, status.Error(codes.NotFound, "")
+	}
+}
+
+func GrpcRouteStream(option OptionStream) grpc.StreamServerInterceptor {
+	return func(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+		if !info.IsServerStream {
+			panic("not server stream")
+		}
+
+		if handler, ok := option.GetHandler(info.FullMethod); ok {
+			return handler(ss)
+		}
+
+		return status.Error(codes.NotFound, "")
 	}
 }
